02302fa19f24501b0f5ff78a866b68e87a5bb621
Saral Bajracharya <rmanbajra@gmail.com>
2024-01-13 19:50:13 +0545

    Font family changed, logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,8 @@ import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { useCart } from './ContextReducer';
 import Modal from '../Modal';
 import Cart from '../screens/Cart';
+import logoImage from './Images/bg.png'
+
 export default function Navbar(props) {
 
     const [cartView, setCartView] = useState(false)
@@ -28,7 +30,10 @@ export default function Navbar(props) {
             <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
                 style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-1 fst-italic" to="/">RaksiPasa:</Link>
+                    <Link className="navbar-brand " to="/">
+                    <img src={logoImage} alt="Logo"
+                    style={{ width: "100px", height: "auto" }} />
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
