781e53ba0b191e58d4c6d0f226f5d705e6a98f5b
Saral Bajracharya <rmanbajra@gmail.com>
2024-01-19 22:03:28 +0545

    Skeleton Loading effect and scale image when hovering added

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,12 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatchCart, useCart } from './ContextReducer'
+import './Card.css';
 // import { Dropdown, DropdownButton } from 'react-bootstrap';
 export default function Card(props) {
   let data = useCart();
-
   let navigate = useNavigate()
+  const [loading, setLoading] = useState(true); // New state for loading status
   const [qty, setQty] = useState(1)
   const [size, setSize] = useState("")
   const priceRef = useRef();
@@ -62,9 +63,15 @@ export default function Card(props) {
   }
 
   useEffect(() => {
-    setSize(priceRef.current.value)
-  }, [options])
+    setSize(priceRef.current.value);
+    // Simulate a delay to show the skeleton loader for a brief period
+    const delayTimer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust the delay time as needed (in milliseconds)
 
+    // Clear the timer when the component unmounts or when the image is loaded
+    return () => clearTimeout(delayTimer);
+  }, [options]);
   // useEffect(()=>{
   // checkBtn();
   //   },[data])
@@ -74,34 +81,36 @@ export default function Card(props) {
   // console.log(totval)
   return (
     <div className='mb-4'>
-    <div className="card mt-3 d-flex flex-column" style={{ maxWidth: "18rem" }}>
-      <img src={props.ImgSrc} className="card-img-top" alt="..." style={{ height: "250px", objectFit: "contain" }} />
-      <div className="card-body">
-        <h5 className="card-title d-flex justify-content-center">{props.foodName}</h5>
-        <div className='container w-100 p-0' style={{ height: "38px" }}>
-          <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} onClick={handleClick} onChange={handleQty}>
-            {Array.from(Array(10), (e, i) => {
-              return (
+      <div className="card mt-3 d-flex flex-column" style={{ maxWidth: "18rem" }}>
+        {/* Use a conditional rendering to show either the image or the skeleton loader */}
+        {loading ? (
+          <div className="skeleton-loader" style={{ height: "250px", width: "100%", backgroundColor: "#ccc" }}></div>
+        ) : (
+          <img src={props.ImgSrc} className="card-img-top" alt="..." style={{ height: "250px", objectFit: "contain" }} />
+        )}
+        <div className="card-body">
+          <h5 className="card-title d-flex justify-content-center">{props.foodName}</h5>
+          <div className='container w-100 p-0' style={{ height: "38px" }}>
+            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} onClick={handleClick} onChange={handleQty}>
+              {Array.from(Array(10), (e, i) => (
                 <option key={i + 1} value={i + 1}>{i + 1}</option>
-              )
-            })}
-          </select>
-          <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} ref={priceRef} onClick={handleClick} onChange={handleOptions}>
-            {priceOptions.map((i) => {
-              return <option key={i} value={i}>{i}</option>
-            })}
-          </select>
-          <div className='d-inline ms-2 h-100 w-20 fs-5'>
-            Rs. {finalPrice}
+              ))}
+            </select>
+            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} ref={priceRef} onClick={handleClick} onChange={handleOptions}>
+              {priceOptions.map((i) => (
+                <option key={i} value={i}>{i}</option>
+              ))}
+            </select>
+            <div className='d-inline ms-2 h-100 w-20 fs-5'>
+              Rs. {finalPrice}
+            </div>
           </div>
-        </div>
-        <br />
-        <div className='d-flex flex-column align-items-center justify-content-center mt-auto'>
-          <button className={'btn btn-success'} onClick={handleAddToCart}>Add to Cart</button>
+          <br />
+          <div className="d-flex flex-column align-items-center justify-content-center mt-auto">
+            <button className={'btn btn-success'} onClick={handleAddToCart}>Add to Cart</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
-//
