5427c3c328031ba0cc20a20c062f04376ce67ff9
Saral Bajracharya <rmanbajra@gmail.com>
2024-02-01 18:29:36 +0545

    Some change in UI of User Profile

--- src/screens/UserProfile.js
+++ src/screens/UserProfile.js
@@ -13,19 +13,21 @@ const UserInfo = () => {
                 <div className="user-info-box">
                     <h2 style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>Your Information</h2>
                     <hr style={{ display: 'inline-block', marginLeft: '10px', width: 'calc(100% - 70px)' }} />
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '8px' }}>
-                            <p style={{ textAlign: 'left', margin: '0' }}><b>Email:</b></p>
-                            <p style={{ textAlign: 'right', margin: '0' }}>{userEmail}</p>
+                    <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
+                        <div style={{ display: 'flex', marginBottom: '8px' }}>
+                            <p style={{ minWidth: '100px', margin: '0' }}><b>Email</b></p>
+                            <p style={{ margin: '0' }}> <b>:</b> &nbsp;{userEmail}</p>
                         </div>
-                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                            <p style={{ textAlign: 'left', margin: '0' }}><b>Username:</b></p>
-                            <p style={{ textAlign: 'right', margin: '0' }}>{userName}</p>
+                        <div style={{ display: 'flex' }}>
+                            <p style={{ minWidth: '100px', margin: '0' }}><b>Username</b></p>
+                            <p style={{ margin: '0' }}> <b>:</b> &nbsp;{userName}</p>
                         </div>
                     </div>
                     {/* Other UI elements */}
                 </div>
             </div>
+
+
         </>
     );
 };
