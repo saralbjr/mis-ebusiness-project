1c9bf07243f14b60b28e714fe750a5e8e7854e2b
Saral Bajracharya <rmanbajra@gmail.com>
2024-02-02 20:02:43 +0545

    show no orders if there are no orders done yet

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-// import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import Loading from './Loading'; // Import the Loading component
+import Loading from './Loading';
 
 export default function MyOrder() {
-    const [orderData, setorderData] = useState({});
+    const [orderData, setOrderData] = useState({});
     const [loading, setLoading] = useState(true);
 
     const fetchMyOrder = async () => {
@@ -20,15 +19,14 @@ export default function MyOrder() {
             });
 
             const data = await response.json();
-            setorderData(data);
+            setOrderData(data);
 
-            setTimeout(()=> {
+            setTimeout(() => {
                 setLoading(false);
-            }, 1500)
-           // Set loading to false after data is fetched
+            }, 1500);
         } catch (error) {
             console.error('Error fetching data:', error);
-            setLoading(false); // Set loading to false in case of an error
+            setLoading(false);
         }
     };
 
@@ -41,52 +39,58 @@ export default function MyOrder() {
             <div>
                 <Navbar />
             </div>
-            <br/> <br/> <br/>
+            <br /> <br /> <br />
             <div className='container'>
                 {loading ? (
-                    // Display loading component while waiting for data
                     <Loading />
                 ) : (
                     <div className='row'>
-                        {orderData !== {} ? Array(orderData).map(data => {
-                            return (
-                                data.orderData ?
-                                    data.orderData.order_data.slice(0).reverse().map((item) => {
-                                        return (
-                                            item.map((arrayData) => {
-                                                return (
-                                                    <div  >
-                                                        {arrayData.Order_date ? <div className='m-auto mt-5'>
-                                                            {data = arrayData.Order_date}
-                                                            <hr />
-                                                        </div> :
-                                                            <div className='col-12 col-md-6 col-lg-3' >
-                                                                <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-                                                                    <div className="card-body">
-                                                                        <h5 className="card-title">{arrayData.name}</h5>
-                                                                        <div className='container w-100 p-0' style={{ height: "38px" }}>
-                                                                            <span className='m-1'>{arrayData.qty}</span>
-                                                                            <span className='m-1'>{arrayData.size}</span>
-                                                                            <span className='m-1'>{data}</span>
-                                                                            <div className=' d-inline ms-2 h-100 w-20 fs-5' >
-                                                                                Rs. {arrayData.price}/-
-                                                                            </div>
-                                                                        </div>
+                        {orderData !== {} ? (
+                            Array(orderData).map(data => (
+                                data.orderData ? (
+                                    data.orderData.order_data.slice(0).reverse().map((item) => (
+                                        item.map((arrayData) => (
+                                            <div>
+                                                {arrayData.Order_date ? (
+                                                    <div className='m-auto mt-5'>
+                                                        {data = arrayData.Order_date}
+                                                        <hr />
+                                                    </div>
+                                                ) : (
+                                                    <div className='col-12 col-md-6 col-lg-3'>
+                                                        <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">{arrayData.name}</h5>
+                                                                <div className='container w-100 p-0' style={{ height: "38px" }}>
+                                                                    <span className='m-1'>{arrayData.qty}</span>
+                                                                    <span className='m-1'>{arrayData.size}</span>
+                                                                    <span className='m-1'>{data}</span>
+                                                                    <div className='d-inline ms-2 h-100 w-20 fs-5'>
+                                                                        Rs. {arrayData.price}/-
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        }
+                                                        </div>
                                                     </div>
-                                                )
-                                            })
-                                        )
-                                    }) : ""
-                            )
-                        }) : ""}
+                                                )}
+                                            </div>
+                                        ))
+                                    ))
+                                ) : (
+                                    <div className="text-center mt-5">
+                                        <h3>No Orders Yet</h3>
+                                    </div>
+                                )
+                            ))
+                        ) : (
+                            <div className="text-center mt-5">
+                                <h3>No Orders Yet</h3>
+                            </div>
+                        )}
                     </div>
                 )}
             </div>
-            <br/>
+            <br />
         </div>
     );
 }
